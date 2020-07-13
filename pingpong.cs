37399357159375aec66df0d1a17cd5079a85c63e
Taylor Phillips <taylorphillips133@gmail.com>
2020-07-13 14:29:12 -0700

    add some guts to the Readme.md to match current project
    Co-Authored-By:  Ian Gregg <iangregg188@gmail.com>

--- pingpong.cs
+++ pingpong.cs
@@ -1,15 +1,26 @@
 using System;
-namespace PingPong  {
-  public class Program  {
-    static void Main(string[] args) {
-      for (int i = 1; i <= 100; i++)  {
-        if (i % 3 == 0 && i % 5 == 0) {
+namespace PingPong  
+{
+  public class Program  
+  {
+    static void Main(string[] args) 
+    {
+      for (int i = 1; i <= 100; i++)  
+      {
+        if (i % 3 == 0 && i % 5 == 0) 
+        {
           Console.WriteLine("Ping Pong");
-        } else if (i % 3 == 0) {
+        } 
+        else if (i % 3 == 0) 
+        {
           Console.WriteLine("Ping");
-        } else if (i % 5 == 0)  {
+        } 
+        else if (i % 5 == 0)  
+        {
           Console.WriteLine("Pong");
-        } else {
+        } 
+        else 
+        {
           Console.WriteLine(i);
         }
       }
